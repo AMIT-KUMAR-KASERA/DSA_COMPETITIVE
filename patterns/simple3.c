7bba06c69446ea7912bc5e9a1f0c2ed692f866ec
amit kumar kasra <amitkumarkasera9999@gmail.com>
2024-03-30 20:55:47 +0530

    patterns

---
+++ patterns/simple3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,num;
+    printf("enter the size of the patterns \n");
+    scanf("%d",&num);
+    for(i=0;i<num;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=1;i<num;i++)
+    {
+        for(j=i;j<num;j++)
+        {
+            printf("*");
+
+        }
+        printf("\n");
+    }
+
+}
