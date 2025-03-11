9ac6f99f306a155965dee6b1f50de6617b2dd2d6
AMIT KUMAR KASERA <106943697+AMIT-KUMAR-KASERA@users.noreply.github.com>
2025-03-12 00:22:21 +0530

    Update 25_02_2025.c

--- C_programming_logic/25_02_2025/25_02_2025.c
+++ C_programming_logic/25_02_2025/25_02_2025.c
@@ -195,3 +195,50 @@ int main()
 }
 */
 
+/* CheckPositiveNegativeOrZero
+Write a C program that checks if a given number is positive, negative, or zero.
+Sample Input
+10
+Sample Output
+Positive
+Explanation
+Use only simple if
+
+Note:
+Your code must be able to print the sample output from the provided sample input. However, your code is run against multiple hidden test cases. Therefore, your code must pass these hidden test cases to solve the problem statement.
+
+Limits
+Memory Limit: 1 MB
+
+Time Limit: 60 ms
+
+Scoring
+Score is assigned if any testcase passes
+
+Allowed Languages
+python, c, csharp, java, javascript
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int num1;
+    scanf("%d",&num1);
+    if(num1>0)
+    {
+        printf("Positive");
+    
+    }
+    if(num1<0)
+    {
+        printf("Negative");
+    }
+    if(num1==0)
+    {
+        printf("Zero");
+    }
+    
+    return 0;
+}
+
