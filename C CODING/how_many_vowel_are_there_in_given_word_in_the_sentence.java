db87999f80b49626e95847a412fca751c28d3565
AMIT KUMAR KASERA <106943697+AMIT-KUMAR-KASERA@users.noreply.github.com>
2026-04-12 15:59:40 +0530

    Update how_many_vowel_are_there_in_given_word_in_the_sentence.java

--- C CODING/how_many_vowel_are_there_in_given_word_in_the_sentence.java
+++ C CODING/how_many_vowel_are_there_in_given_word_in_the_sentence.java
@@ -3,5 +3,26 @@ write a program to count how many words start with a vowel (a, e, i, o, u).
 
 Amit is an excellent engineer
 
-5
+
+class Main {
+    public static void main(String[] args) {
+        String str="Amit is an excellent engineer";
+        int count=0;
+        
+        str=str.toUpperCase();
+        String[] words=str.split(" ");
+        for(int i=0;i<words.length;i++)
+        {
+            char firstChar=words[i].charAt(0);
+            if(firstChar=='A'||firstChar=='E'||firstChar=='I'||firstChar=='O'||firstChar=='U')
+            {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+
+
 
