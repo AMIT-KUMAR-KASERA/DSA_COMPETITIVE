8cc3f64d7f17c66a06664076512512bd7d35b7d5
AMIT KUMAR KASERA <106943697+AMIT-KUMAR-KASERA@users.noreply.github.com>
2026-04-18 23:00:53 +0530

    Update print_last_non_reapeated_charcter.java

--- C CODING/print_last_non_reapeated_charcter.java
+++ C CODING/print_last_non_reapeated_charcter.java
@@ -1,1 +1,24 @@
-
+class Main {
+    public static void main(String[] args) {
+        String str="Software services";
+       String result="";
+       for(int i=str.length()-1;i>0;i--)
+       {
+           int count=1;
+           if(str.charAt(i)!=' '){
+           for(int j=i-1;j>0;j--)
+           {
+               if(str.charAt(i)==str.charAt(j)){
+                   count++;
+                   break;
+               }
+           }
+           if(count==1){
+               result+=str.charAt(i);
+               System.out.println(result);
+               break;
+           }
+           }
+       }
+    }
+}
